08188c0e3f614872647ba8291617d0af2295aaf6
jhenrynyc <jhenry012@citymail.cuny.edu>
2023-12-13 11:08:40 -0500

    UIFinish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.sadiq.planner"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sadiq.planner"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -39,4 +39,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.10.0")
 }
--- app/src/main/java/com/sadiq/planner/MainActivity.java
+++ app/src/main/java/com/sadiq/planner/MainActivity.java
@@ -1,13 +1,82 @@
 package com.sadiq.planner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    public List<Event> eventList = new ArrayList<>();
+
+
+    EditText eventName;
+    EditText startTime;
+    EditText endTime;
+    EditText location;
+    EditText day;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.newEventButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event event = new Event();
+                eventName = findViewById(R.id.newEventText);
+                startTime = findViewById(R.id.newEventStart);
+                endTime = findViewById(R.id.newEventEnd);
+                location = findViewById(R.id.newEventLocation);
+                day = findViewById(R.id.newEventDay);
+                event.setName(eventName.getText().toString());
+                event.setLocation(location.getText().toString());
+                String dayString = day.getText().toString();
+                String startTimeString = startTime.getText().toString();
+                String endTimeString = endTime.getText().toString();
+                event.setEndTime(Integer.parseInt(endTimeString));
+                event.setStartTime(Integer.parseInt(startTimeString));
+                event.setDay(Integer.parseInt(dayString));
+                eventList.add(event);
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+
+        }
+
+
+
+
+
+
+
     }
-}
+
+
