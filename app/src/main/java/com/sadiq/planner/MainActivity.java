eccc0c84418974f6bb86e49c3966df3deb705e03
Sadiq <sadiqsobur@gmail.com>
2023-12-13 00:13:11 -0500

    Stretched out the friday block a bit, also made it so that each time block has a different color.

--- app/src/main/java/com/sadiq/planner/MainActivity.java
+++ app/src/main/java/com/sadiq/planner/MainActivity.java
@@ -3,26 +3,19 @@ package com.sadiq.planner;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
-import com.google.android.material.tabs.TabLayout;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -37,20 +30,22 @@ public class MainActivity extends AppCompatActivity {
         List<Event> events = new ArrayList<>(); // example events for testing
         events.add(new Event("Math", "NAC", 8, 13, 2));
         events.add(new Event("English", "Shepard", 4, 10, 3));
-        events.add(new Event("Science", "Steinman", 2, 6, 2));
+        events.add(new Event("CSC 221", "Steinman", 2, 6, 2));
         events.add(new Event("Physics", "Steinman", 2, 4, 4));
         events.add(new Event("Art", "Steinman", 6, 7, 5));
-        events.add(new Event("Social Studies", "Steinman", 8, 16, 1));
+        events.add(new Event("Science", "MR-3", 8, 16, 1));
 
         populateTable(tableLayout);
         Resources res = getResources(); // resources
         int[] colors = res.getIntArray(R.array.rainbow);
         for (Event e : events) {
+            int cooler = colors[randColor()];
             for (int i = e.getStart(); i < e.getEnd(); i++) {
                 // TextView creation and configuration
                 TextView text = (TextView) getCell(tableLayout, i, e.getDay());
                 text.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
-                text.setMaxWidth(30);
+                text.setEllipsize(TextUtils.TruncateAt.END);
+                text.setWidth(30);
                 text.setAutoSizeTextTypeUniformWithConfiguration(10, 16, 1, TypedValue.COMPLEX_UNIT_DIP);
                 text.setLines(2);
                 text.setPadding(10, 10, 10, 0);
@@ -60,13 +55,19 @@ public class MainActivity extends AppCompatActivity {
                     text.setText(t);
                     text.setTextColor(getColor(R.color.white));
                 }
-                text.setBackgroundColor(colors[e.getDay() + 2]);
+
+                text.setBackgroundColor(cooler);
             }
         }
 
         setContentView(constraintLayout);
     }
 
+    private int randColor() // NOT BLACK OR WHITE
+    {
+        Random rand = new Random();
+        return rand.nextInt(13) + 2;
+    }
     private void populateTable(TableLayout tableLayout)
     {
         for (int i = 0; i < 17; i++)
